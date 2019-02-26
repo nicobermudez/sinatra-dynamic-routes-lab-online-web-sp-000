e707c886e494bdef4cc5a2564573c26a7af4a441
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-26 19:25:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
-  get 'reversename/:name' do
+  get '/reversename/:name' do
+  end
+
+  get '/square/:number' do 
   end
 
 end
