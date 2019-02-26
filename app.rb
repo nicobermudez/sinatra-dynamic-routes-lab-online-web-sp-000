6efadf335d4afe472e2ad1bf8562ca7753cf05e4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-26 19:32:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    "#{params[:number].times {|i| @phrase.to_s}}"
+    "#{params[:number].to_i.times {|i| @phrase.to_s}}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
