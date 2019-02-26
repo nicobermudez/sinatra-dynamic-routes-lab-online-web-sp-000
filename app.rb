0dc7464ccfa71998f993a9b5d9f0d3d5e82a8bfa
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-26 19:40:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,6 +24,12 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i 
+    number2 = params[:number2].to_i
+    
+    case params[:operation]
+    when 'add'
+      
   end
 
 end
